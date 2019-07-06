c183ed154c67c3a6df255b6828167f18b3a54aa6
Martin Crabtree <martin.crabtree@gmail.com>
2019-07-06 14:12:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,17 +1,18 @@
 def new_hash
-  # return an empty hash
+  newHash = {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  newHash = {name: "bob"}
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  newHash = {name: "Grace Hopper"}
 end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
+  newHash = {myInt: 
 end
 
 def my_hash_creator(key, value)
